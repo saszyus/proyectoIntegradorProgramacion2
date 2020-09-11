4d6d66a0bcbab745f12880ae896fee77ece29390
Sasa <safdiesantiago98@gmail.com>
2020-09-11 14:19:52 -0300

    /home y CSS conectado en HOME!

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 let homeController = require("../controllers/homeController")
 
-router.get("/home", homeController.home);
+router.get("/", homeController.home);
 
 module.exports = router;
