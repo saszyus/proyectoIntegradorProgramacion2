c306f34cd72566f84d30a5dafd44aecc383a84d0
Sasa <safdiesantiago98@gmail.com>
2020-09-14 10:05:27 -0300

    PaqueteFrutas Basico

---
+++ frutas.js
@@ -0,0 +1,86 @@
+var frutas = {
+
+
+    lista: [
+        {
+
+        nombre: "Manzana",
+        id:1,
+        
+    },
+
+
+    {
+
+        nombre: "Naranja",
+        id:2,
+       
+    },
+
+
+
+    {
+
+        nombre: "Pera",
+        id:3,
+  
+    },
+
+    {
+
+        nombre: "Uva",
+        id:4,
+    },
+
+    {
+
+        nombre: "Frutilla",
+        id:5,
+
+    },
+
+    {
+
+        nombre: "Sandia",
+        id:6,
+
+    },
+
+    {
+
+
+        nombre: "Melon",
+        id:7,
+    },
+
+    {
+
+
+        nombre: "Durazno",
+        id:8,
+
+    },
+
+    {
+
+
+        nombre: "Mango",
+        id:9,
+
+    },
+
+      {
+
+
+        nombre: "Mandarina",
+        id:10,
+
+    },
+
+    ]
+
+
+}
+
+
+module.exports = frutas;
