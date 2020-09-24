282fcc1a9ee61e28fa4413286b52c5a6b05572f0
Sasa <safdiesantiago98@gmail.com>
2020-09-24 16:48:26 -0300

    cambio en el for de list

--- frutas.js
+++ frutas.js
@@ -5,7 +5,9 @@ var frutas = {
 
         nombre: "Manzana",
         user:"@Manzana_Roja",
+        fechaNacimiento:"24/08/1996",
         id:1,
+        edad:24 ,
         
     },
 
@@ -14,7 +16,9 @@ var frutas = {
 
         nombre: "Naranja",
         user:"@Naranja_Exprimida98",
+        fechaNacimiento:"01/02/1999",
         id:2,
+        edad: 21,
        
     },
 
@@ -22,7 +26,9 @@ var frutas = {
 
         nombre: "Pera",
         user:"@laPeraQueTeEspera;)",
+        fechaNacimiento:"02/02/1997",
         id:3,
+        edad:23 ,
   
     },
 
@@ -30,22 +36,30 @@ var frutas = {
 
         nombre: "Uva",
         user:"@uvaDeVino",
+        fechaNacimiento:"09/10/1999",
         id:4,
+        edad:21 ,
+    },
+
+  
+
+    {
+
+        nombre: "Sandia",
+        user:"@Sandia-->cOOL",
+        fechaNacimiento:"01/12/1999",
+        id:6,
+        edad: 20,
+
     },
 
     {
 
         nombre: "Frutilla",
         user:"@frufru",
+        fechaNacimiento:"01/02/2005",
         id:5,
-
-    },
-
-    {
-
-        nombre: "Sandia",
-        user:"@Sandia-->cOOL",
-        id:6,
+        edad:15 ,
 
     },
 
@@ -54,7 +68,9 @@ var frutas = {
 
         nombre: "Banana",
         user:"@bananita_Aa",
+        fechaNacimiento:"04/10/1900",
         id:7,
+        edad: 220,
     },
 
     ]
