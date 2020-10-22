48ef22909581ad27b558c359850aef7c5e24cee9
Sasa <sasa>
2020-10-22 19:50:27 -0300

    puerto

--- database/config/config.js
+++ database/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "database": "database_development",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "3306"
+    "port": "8889"
   },
   "test": {
     "username": "root",
