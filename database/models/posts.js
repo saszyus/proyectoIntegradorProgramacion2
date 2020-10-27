47d51c374622efe824f48f7fded3e519afe1e424
Sasa <safdiesantiago98@gmail.com>
2020-10-27 10:14:42 -0300

    agregar post listo

--- database/models/posts.js
+++ database/models/posts.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
 
         },
         texto_creacion: {
-            type: DataTypes.STRING
+            type: DataTypes.DATE
 
         },
         url_imagen: {
