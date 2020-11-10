578381516d3293e31fab8b09231f9ac64cf8cded
Iara <iburgueno@udesa.edu.ar>
2020-11-10 15:57:38 -0300

    electiva registracion

--- public/javascripts/registracion.js
+++ public/javascripts/registracion.js
@@ -1,21 +1,21 @@
+window.onload = function (){
 
-let form  = document.getElementById('registracion');
+var registracion = document.querySelector(".registracion");
+registracion.onsubmit = function(validar) {
+    validar.preventDefault();
 
-form.addEventListener('submit', (validacion) => {
-    validacion.preventDefault();
-
-    var nombreUsuario = getElementById('usuario').value;
-    var email = getElementById('email').value;
-    var contra = getElementById('pass').value;
-    var fechaNacimiento = getElementById('fechaNacimiento').value;
-    var url = getElementById('url').value;
-    var pregunta = getElementById('pregunta').value;
-    var respuesta = getElementById ('respuesta').value;
+    var nombreUsuario = document.getElementById('usuario').value;
+    var email = document.getElementById('email').value;
+    var contra = document.getElementById('pass').value;
+    var fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    var url = document.getElementById('url').value;
+    var pregunta = document.getElementById('pregunta').value;
+    var respuesta = document.getElementById ('respuesta').value;
 
 
     if (nombreUsuario == "" || email == "" || contra == "" || fechaNacimiento == "" || url == "" || pregunta == "" || respuesta == "") {
         document.querySelector(".error").innerHTML= 
-         "<div class='alert alert-danger' role='alert'> Todos los campos son obligatorios </div>"
+         "<div class='uk-alert-danger alert' uk-alert> <a class='uk-alert-close' uk-close></a><p> Todos los campos son obligatorios </p> </div>"
       setTimeout(function (){
           document.querySelector(".alert").style.display = "none"
       },3000)
@@ -23,5 +23,5 @@ form.addEventListener('submit', (validacion) => {
     }
 
 
-})
+} }
 
