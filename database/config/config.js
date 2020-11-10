5745d148daeb27d6962f43b73722ed1b788250da
Iara <iburgueno@udesa.edu.ar>
2020-11-10 09:24:04 -0300

    validacion de registracion y arreglos

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "root",
     "database": "fruitbook",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "3306"
+    "port": "8889"
   },
   "test": {
     "username": "root",
---
+++ public/javascripts/registracion.js
@@ -0,0 +1,21 @@
+function validar() {
+    var nombreUsuario = getElementById('usuario').value;
+    var email = getElementById('email').value;
+    var contra = getElementById('pass').value;
+    var fechaNacimiento = getElementById('fechaNacimiento').value;
+    var url = getElementById('url').value;
+    var pregunta = getElementById('pregunta').value;
+    var respuesta = getElementById ('respuesta').value;
+
+
+    if (nombreUsuario == "" || email == "" || contra == "" || fechaNacimiento == "" || url == "" || pregunta == "" || respuesta == "") {
+        document.querySelector(".error").innerHTML= 
+         "<div class='alert alert-danger' role='alert'> Todos los campos son obligatorios </div>"
+      setTimeout(function (){
+          document.querySelector(".alert").style.display = "none"
+      },3000)
+        
+    }
+
+
+}
