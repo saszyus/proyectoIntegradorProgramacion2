b07bf478e515414ab02ca22e8d0f994b21d34490
Sasa <safdiesantiago98@gmail.com>
2020-11-11 21:15:42 -0300

    Mi Perfil terminando tambien

--- Controllers/profileController.js
+++ Controllers/profileController.js
@@ -7,7 +7,16 @@ let profileController =  {
 
         // hay que hacer un if para verificar si hay un usuario logueado
  
-        db.Usuario.findByPk(req.session.usuarioLogueado.id)
+        db.Usuario.findByPk(req.session.usuarioLogueado.id,{
+
+            include:[
+
+                {all:true,nested:true}
+
+            ],
+
+
+        })
         .then(function(perfil){
            res.render("miPerfil",{perfil:perfil})
         })
